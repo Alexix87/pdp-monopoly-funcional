4a3c0f6338ace64498734867e4bcf34dcad05d6a
Alexix87 <alexis.cabo.87@gmail.com>
2021-05-11 20:28:37 -0300

    listas las acciones

--- monopoly/src/Monopoly.hs
+++ monopoly/src/Monopoly.hs
@@ -1,14 +1,9 @@
 import Text.Show.Functions
 
-montoInicial :: Int
-montoInicial = 500
-
-{-- tipos de datos: --}
-
 data Propiedad = Propiedad{
     nombreDePropiedad :: String,
     precio :: Int
-} deriving (Show)
+} deriving (Show, Eq)
 
 data Participante = Participante {
     nombreDeParticipante :: String,
@@ -21,6 +16,9 @@ data Participante = Participante {
 type Accion = (Participante -> Participante)
 
 type Tactica = String
+
+montoInicial :: Int
+montoInicial = 500
 
 accionista :: Tactica
 accionista = "Accionista"
@@ -37,8 +35,9 @@ agregarAccion unaAccion unParticipante = unParticipante { acciones =(acciones un
 aumentaDinero :: Int -> Participante -> Participante
 aumentaDinero unMonto unParticipante = unParticipante { dinero = (+unMonto).dinero $ unParticipante } 
 
- 
-{--acciones: --}
+disminuyeDinero :: Int -> Participante -> Participante
+disminuyeDinero unMonto unParticipante = unParticipante { dinero = (dinero unParticipante) - unMonto }
+
 pasarPorElBanco :: Accion
 pasarPorElBanco unParticipante = aumentaDinero 40 unParticipante {tactica = compradorCompulsivo}
 
@@ -62,9 +61,35 @@ subastar unaPropiedad unParticipante
     | esGanadorDeSubasta unParticipante = comprarPropiedad unaPropiedad unParticipante
     | otherwise = unParticipante
 
+esAccionista :: Participante -> Bool
+esAccionista unParticipante = (==accionista).tactica $ unParticipante
 
 pagarAAccionistas :: Accion
-pagarAAccionistas unParticipante  = unParticipante
+pagarAAccionistas unParticipante 
+    | esAccionista unParticipante = aumentaDinero 200 unParticipante
+    | otherwise = disminuyeDinero 100 unParticipante
+
+esPropiedadBarata :: Propiedad -> Bool
+esPropiedadBarata unaPropiedad = (<150).precio $unaPropiedad
+
+valorDeAlquiler :: Propiedad -> Int
+valorDeAlquiler unaPropiedad
+    | esPropiedadBarata unaPropiedad = 150
+    | otherwise = 200
+
+cobrarAlquileres :: Accion
+cobrarAlquileres unParticipante = (flip aumentaDinero unParticipante).sum.(map valorDeAlquiler).propiedadesCompradas $ unParticipante
+
+esDueño :: Propiedad -> Participante -> Bool
+esDueño unaPropiedad unParticipante = any (==unaPropiedad) $ propiedadesCompradas unParticipante
+
+quitarAccion :: Participante -> Participante
+quitarAccion unParticipante = unParticipante { acciones = tail.acciones $ unParticipante }
+
+hacerBerrinchePor :: Propiedad -> Accion
+hacerBerrinchePor unaPropiedad unParticipante
+    | not.esDueño unaPropiedad $ unParticipante =  (aumentaDinero 10).agregarAccion gritar $ unParticipante
+    | otherwise = quitarAccion unParticipante
 
 carolina :: Participante
 carolina = Participante "Carolina" montoInicial accionista [] [pasarPorElBanco, pagarAAccionistas] 
@@ -72,12 +97,16 @@ carolina = Participante "Carolina" montoInicial accionista [] [pasarPorElBanco, 
 manuel :: Participante
 manuel = Participante "Manuel" montoInicial oferenteSingular [] [pasarPorElBanco, enojarse]
 
+
 {- Para pruebas: -}
 cordoba :: Propiedad
 cordoba = Propiedad "Cordoba" 200
 
+misiones :: Propiedad
+misiones = Propiedad "Misiones" 100
+
 juan :: Participante 
-juan = Participante "Juan" montoInicial compradorCompulsivo [] [subastar cordoba]
+juan = Participante "Juan" montoInicial accionista [misiones,cordoba] [hacerBerrinchePor cordoba]
 
 juega :: Participante -> Participante
 juega unParticipante = acciones unParticipante !! 0 $ unParticipante
